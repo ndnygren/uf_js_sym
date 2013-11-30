e48412dc9a6eb0c56935f22d8f6b7f0c6625eb3a
Nick Nygren <nnygren@shaw.ca>
2013-11-30 12:42:38 -0700

    some documentation for parse.js

--- js/parse.js
+++ js/parse.js
@@ -17,6 +17,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+// trim leading and trailing whitespace
 function trim(input)
 {
 	if (typeof(input) != 'string') { return ""; }
@@ -24,6 +26,8 @@ function trim(input)
 	return input.replace(/\s+$/, "").replace(/^\s+/, "");
 }
 
+
+// break (explode) sting into array based on given delimiter
 function breakOnDelim(input, delim)
 {
 	var i;
@@ -45,6 +49,8 @@ function breakOnDelim(input, delim)
 	return output;
 }
 
+
+// based on line-break delimited list of patterns, create array of pattern arrays
 function buildPatternList(input)
 {
 	if (typeof(input) != 'string') { return []; }
@@ -54,6 +60,7 @@ function buildPatternList(input)
 
 	for (i = 0; i < temp.length; i++)
 	{
+		// ignore blank or whitespace only lines
 		if (trim(temp[i]) != "")
 		{
 			output.push(patToArray(temp[i]));
@@ -63,7 +70,7 @@ function buildPatternList(input)
 	return output;
 }
 
-
+// converts a pattern string into a pattern array, detecting "AV" and "LV"
 function patToArray(input1)
 {
 	var i = 0;
@@ -90,6 +97,7 @@ function patToArray(input1)
 	return output;
 }
 
+// ensures proper bracking for both "()" and "{}"
 function testBracketBalance(input)
 {
 	var i;
@@ -115,6 +123,8 @@ function testBracketBalance(input)
 	return false;
 }
 
+
+// given a pattern and unparsed node, attempt to apply the pattern to the nodes raw data
 function quickParse(pata, output)
 {
 	var input = trim(output.data);
@@ -123,24 +133,29 @@ function quickParse(pata, output)
 
 	while (i < input.length && j < pata.length)
 	{
+		// tentatively look past variables
 		if (pata[j] == "LV" || pata[j] == "AV")
 		{
 			j++;
 		}
+		// if the node's data does not start with the same character as the pattern, fail
 		else if (j == 0 && i != 0)
 		{
 			output.clearSub();
 			output.toUn();
 			return false;
 		}
+		// too few charaters to complete the pattern, fail
 		else if (i + pata[j].length > input.length)
 		{
 			output.clearSub();
 			output.toUn();
 			return false;
 		}
+		// substring matches pattern, store match and continue to next pattern 
 		else if (input.substr(i, pata[j].length) == pata[j])
 		{
+			// if this is the first pattern piece, do not attempt to read data for variable
 			if (j == 0)
 			{
 				output.addToken(pata[j]);
@@ -148,9 +163,11 @@ function quickParse(pata, output)
 				last = i;
 				j++;
 			}
+			// else, look for a string to complete the sub-variables
 			else if (pata[j-1] == "AV" || pata[j-1] == "LV")
 			{
 				candidate = input.substr(last, i - last);
+				// ingore the match if the resulting variable would have unbalanced brackets
 				if (trim(candidate) != "" && testBracketBalance(candidate))
 				{
 					output.addUnParsed(candidate);
@@ -161,6 +178,7 @@ function quickParse(pata, output)
 				}
 				else
 				{
+					// by-pass the false postive match
 					i++;
 				}
 			}
@@ -173,11 +191,13 @@ function quickParse(pata, output)
 
 	//"i: " + i + "/" + input.length + "\nj: " + j + "/" + pata.length + "\n" + JSON.stringify(input) + "\n" + JSON.stringify(pata) + "\n"
 
+	// collect data for trailing variable
 	if (pata.length == j && pata[j-1] == "AV" || pata[j-1] == "LV")
 	{
 		candidate = input.substr(last);
 		output.addUnParsed(candidate);
 	}
+	// detect case where either input or pattern is not fully used
 	else if (j < pata.length || i < input.length)
 	{
 		output.clearSub();
@@ -185,11 +205,13 @@ function quickParse(pata, output)
 		return false;
 	}
 
+	// set type to success case
 	output.pat = pata;
 	output.toInner();
 	return true;
 }
 
+// recursively apply quickParse, to fully parse a string using an array of patterns
 function fullParse(input, apatterns, lpatterns)
 {
 	var node = new FlexibleNode(), current;
@@ -200,6 +222,7 @@ function fullParse(input, apatterns, lpatterns)
 	node.toUn();
 	node.data = input;
 
+	// explicitly represent recursion with a stack
 	stack.push({node: node, type: "both"});
 
 	while (stack.length != 0)
@@ -209,10 +232,13 @@ function fullParse(input, apatterns, lpatterns)
 		log += "<br/>\n";
 		i = 0;
 
+		// decide pattern set to use based on previously matched pattern
 		if (current.type == "AV") { patterns = apatterns; }
 		else if (current.type == "LV") { patterns = apatterns; }
+		// merge pattern sets where no previous pattern was used
 		else if (current.type == "both") { patterns = lpatterns.concat(apatterns); }
 
+		// try/fail patterns in order until one succeeds
 		while(i < patterns.length && !quickParse(patterns[i], current.node))
 		{
 			log += ("\"" + JSON.stringify(patterns[i]) + "\" failed for " + JSON.stringify(current));
@@ -220,13 +246,14 @@ function fullParse(input, apatterns, lpatterns)
 			i++;
 		}
 		if (patterns.length > i) { log += ("\"" + JSON.stringify(patterns[i]) + "\" succeeded for " + JSON.stringify(current) + "<br/>\n"); }
-
+		// push unparsed child nodes onto the stack and recursed
 		for (i = 0; i < current.node.sub.length; i++)
 		{
 			if (current.node.sub[i].isUn()) { stack.push({node: current.node.sub[i], type: current.node.pat[i]} ); }
 		}
 	}
 
+	// return combined result (all sub-nodes passed to stack by reference, so result is stored in the original node)
 	return node.clean();
 }
 
